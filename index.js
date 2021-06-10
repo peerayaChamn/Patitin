b6edd2bc90b2d22ff0e1ba8c6e040c24a7299ecf
Lau1259 <62459638+Lau1259@users.noreply.github.com>
2021-06-10 00:42:38 -0300

    added scss stuff including brief overview

--- index.js
+++ index.js
@@ -13,5 +13,5 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/scss', (req, res) => res.render('pages/scss-101'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
-  
