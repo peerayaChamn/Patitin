149f8a347bcdf76f0af5d75ae7bceb9ea60f7ed0
DELFINA MONCAYO <mon19012@byui.edu>
2021-06-22 00:04:12 -0400

    Adding group project as /test

--- routes/home.js
+++ routes/home.js
@@ -3,12 +3,13 @@ const express = require('express');
 
 
 const homeController = require('../controllers/home');
-// const calendarController = require('../controllers/calendar');
+const calendarController = require('../controllers/calendar');
 const isAuth = require('../middleware/is-auth');
 
 const router = express.Router();
 
 router.get('/', homeController.getIndex);
 router.get('/calendar', homeController.showCalendar);
+router.get('/test', calendarController.getTest);
 
 module.exports = router;
